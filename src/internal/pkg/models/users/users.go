d6d46e621feeae5a6ba5901ce6b9e32e0ed0a4f2
Masaru <hittec9609@gmail.com>
2023-02-13 09:28:40 +0900

    [Add] ユーザの新規作成関数を追加

---
+++ src/internal/pkg/models/users/users.go
@@ -0,0 +1,26 @@
+package users
+
+import (
+	"database/sql"
+	"log"
+)
+
+/* ユーザの新規作成 */
+func CreateUser(db *sql.DB, userName string, hashedPassword string) error {
+	sqlStatement := "INSERT INTO users(name, password) VALUES(?, ?)"
+
+	stmt, err := db.Prepare(sqlStatement)
+	if err != nil {
+		log.Printf("Failed to Prepare for create retrieval operation in the users: %v", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(userName, hashedPassword)
+	if err != nil {
+		log.Printf("Failed to Exec for create retrieval operation in the users: %v", err)
+		return err
+	}
+
+	return err
+}
