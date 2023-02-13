7e9cb9a389befac07396dbf7e3b9913d97fd3c65
Masaru <hittec9609@gmail.com>
2023-02-13 10:31:31 +0900

    [Add] 文字列にクオーテーションが混入されているかのチェックを行う関数を追加

--- src/internal/pkg/strings/strings.go
+++ src/internal/pkg/strings/strings.go
@@ -13,3 +13,15 @@ func CheckWhitespaceInString(stringData string) bool {
 
 	return checkResult
 }
+
+/* 文字列にクオーテーションが混入されているかのチェックを行う */
+func CheckQuotationInString(stringData string) bool {
+	// 特殊文字/空白などの区切り文字の指定と初期化
+	reg := "[\"'`”’｀]"
+	initializedReg := regexp.MustCompile(reg)
+
+	// 文字列のチェック
+	checkResult := initializedReg.MatchString(stringData)
+
+	return checkResult
+}
