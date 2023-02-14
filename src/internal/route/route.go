af51b2258ad09dd983622bca0360f646774ce281
Masaru <hittec9609@gmail.com>
2023-02-14 15:32:52 +0900

    [Fix] アカウント情報の箇所を修正

--- src/internal/route/route.go
+++ src/internal/route/route.go
@@ -25,7 +25,7 @@ func InitRouting(db *sql.DB) *echo.Echo {
 
 	/* Unauthorized routing group. */
 	unAuthenticatedGroup := e.Group("/auth")
-	unAuthenticatedGroup.Use(auth.UnAuthenticatedMiddleware)
+	// unAuthenticatedGroup.Use(auth.UnAuthenticatedMiddleware)
 	unAuthenticatedGroup.GET("/signup", templates.SignupPage)
 	unAuthenticatedGroup.POST("/signup", account.Signup(db))
 	unAuthenticatedGroup.GET("/login", templates.LoginPage)
