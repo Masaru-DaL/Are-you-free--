e8f7ded2999f6964bd275d61c2695a8cf9371f79
Masaru <hittec9609@gmail.com>
2023-02-23 05:37:08 +0900

    [fix] numパッケージの関数を削除した

--- src/internal/pkg/num/num.go
+++ src/internal/pkg/num/num.go
@@ -1,17 +1,1 @@
 package num
-
-import (
-	"fmt"
-	"strconv"
-)
-
-func NumToFormattedString(num int) string {
-	var str string
-	if num < 10 {
-		str = fmt.Sprintf("0%d", num)
-	} else {
-		str = strconv.Itoa(num)
-	}
-
-	return str
-}
